b081ec116a69186bd8acf3dcf25bf29e2717cf50
yuto <tanaka.yuto.3002@gmail.com>
2021-01-21 15:36:50 +0900

    Topの内容変更

--- src/screens/Project/Top.tsx
+++ src/screens/Project/Top.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Project, Words, List } from 'arwes';
+import { Project, Words, Frame } from 'arwes';
 import { Anim } from '../../data/model/Anim';
 
 const Top: React.FC = () => {
@@ -7,72 +7,197 @@ const Top: React.FC = () => {
     <Project animate header="aweconについて">
       {(anim: Anim) => (
         <div>
-          <h2>
-            <Words animate show={anim.entered}>
-              使用方法
-            </Words>
-          </h2>
-          <h3>
-            <Words animate show={anim.entered}>
-              エアコン起動方法
-            </Words>
-          </h3>
-          <List node="ol">
-            <li>
-              <p>
-                <Words animate show={anim.entered}>
-                  ・温度(16~29℃)
-                </Words>
-              </p>
-              <p>
-                <Words animate show={anim.entered}>
-                  ・運転モード(オート、暖房、冷房、除湿)
-                </Words>
-              </p>
-              <p>
-                <Words animate show={anim.entered}>
-                  ・風量(自動、1、2、3、4、5)
-                </Words>
-              </p>
-              <p>
-                <Words animate show={anim.entered}>
-                  ・風向高さ(自動、スイング、天井、上向き、やや上向き、やや下向き、下向き)
-                </Words>
-              </p>
-              <p>
-                <Words animate show={anim.entered}>
-                  ・風向左右(自動、スイング、左向き、やや左向き、真ん中、やや右向き、右向き)
-                </Words>
-              </p>
-              <p>
-                <Words animate show={anim.entered}>
-                  の計5項目を設定する。
-                </Words>
-              </p>
-            </li>
-            <li>
-              <p>
-                <Words animate show={anim.entered}>
-                  Arduinoに接続されている赤外線LEDを、エアコンに向け、「エアコンを起動」ボタンを押す。
-                </Words>
-              </p>
-            </li>
-          </List>
+          {/* 吹き出し1 */}
+          <div>
+            <div
+              style={{
+                display: 'inline-block',
+                borderRadius: '10px 10px 10px 10px',
+                borderStyle: 'solid',
+                borderWidth: '2 px',
+                borderColor: 'rgb(30, 60, 150) rgb(10, 30, 100) rgb(10, 30, 100) rgb(30, 60, 150)',
+                backgroundColor: 'rgba(30, 60, 100, 0.4)',
+                padding: '10px 20px',
+                margin: '0 10px 10px 10px',
+              }}
+            >
+              <Words animate show={anim.entered}>
+                「エアコンをちゃんと消したか心配...」
+              </Words>
+            </div>
+            <div
+              style={{
+                display: 'inline-block',
+                width: '20px',
+                height: '20px',
+                borderRadius: '50%',
+                border: 'solid 1px rgb(30, 60, 100)',
+                backgroundColor: 'rgba(30, 60, 100, 0.4)',
+                marginRight: '10px',
+                marginBottom: '10px',
+                verticalAlign: 'bottom',
+              }}
+            ></div>
+            <div
+              style={{
+                display: 'inline-block',
+                width: '13px',
+                height: '13px',
+                borderRadius: '50%',
+                border: 'solid 1px rgb(30, 60, 100)',
+                backgroundColor: 'rgba(30, 60, 100, 0.4)',
+                marginRight: '10px',
+                marginBottom: '10px',
+                verticalAlign: 'bottom',
+              }}
+            ></div>
+            <div
+              style={{
+                display: 'inline-block',
+                width: '8px',
+                height: '8px',
+                borderRadius: '50%',
+                border: 'solid 1px rgb(30, 60, 100)',
+                backgroundColor: 'rgba(30, 60, 100, 0.4)',
+                marginBottom: '10px',
+                verticalAlign: 'bottom',
+              }}
+            ></div>
+          </div>
 
-          <h3>
+          {/* 吹き出し2 */}
+          <div>
+            <div
+              style={{
+                display: 'inline-block',
+                borderRadius: '10px 10px 10px 10px',
+                borderStyle: 'solid',
+                borderWidth: '2 px',
+                borderColor: 'rgb(100, 60, 100) rgb(70, 40, 70) rgb(70, 40, 70) rgb(100, 60, 100)',
+                backgroundColor: 'rgba(100, 60, 100, 0.4)',
+                padding: '10px 20px',
+                margin: '0 10px 10px 60px',
+              }}
+            >
+              <Words animate show={anim.entered}>
+                「帰ってから暖房つけてもあったまるまでが寒すぎる!!」
+              </Words>
+            </div>
+            <div
+              style={{
+                display: 'inline-block',
+                width: '20px',
+                height: '20px',
+                borderRadius: '50%',
+                border: 'solid 1px rgb(100, 60, 100)',
+                backgroundColor: 'rgba(100, 60, 100, 0.4)',
+                marginRight: '10px',
+                marginBottom: '10px',
+                verticalAlign: 'bottom',
+              }}
+            ></div>
+            <div
+              style={{
+                display: 'inline-block',
+                width: '13px',
+                height: '13px',
+                borderRadius: '50%',
+                border: 'solid 1px rgb(100, 60, 100)',
+                backgroundColor: 'rgba(100, 60, 100, 0.4)',
+                marginRight: '10px',
+                marginBottom: '10px',
+                verticalAlign: 'bottom',
+              }}
+            ></div>
+            <div
+              style={{
+                display: 'inline-block',
+                width: '8px',
+                height: '8px',
+                borderRadius: '50%',
+                border: 'solid 1px rgb(100, 60, 100)',
+                backgroundColor: 'rgba(100, 60, 100, 0.4)',
+                marginRight: '10px',
+                marginBottom: '10px',
+                verticalAlign: 'bottom',
+              }}
+            ></div>
+          </div>
+
+          {/* 吹き出し3 */}
+          <div>
+            <div
+              style={{
+                display: 'inline-block',
+                borderRadius: '10px 10px 10px 10px',
+                borderStyle: 'solid',
+                borderWidth: '2 px',
+                borderColor: 'rgb(50, 120, 100) rgb(35, 80, 70) rgb(35, 80, 70) rgb(50, 120, 100)',
+                backgroundColor: 'rgba(50, 120, 100, 0.4)',
+                padding: '10px 20px',
+                margin: '0 10px 10px 20px',
+              }}
+            >
+              <Words animate show={anim.entered}>
+                「リモコンを取りに行くのは面倒だけど、スマホなら手元にある」
+              </Words>
+            </div>
+            <div
+              style={{
+                display: 'inline-block',
+                width: '20px',
+                height: '20px',
+                borderRadius: '50%',
+                border: 'solid 1px rgb(50, 120, 100)',
+                backgroundColor: 'rgba(50, 120, 100, 0.4)',
+                marginRight: '10px',
+                marginBottom: '10px',
+                verticalAlign: 'bottom',
+              }}
+            ></div>
+            <div
+              style={{
+                display: 'inline-block',
+                width: '13px',
+                height: '13px',
+                borderRadius: '50%',
+                border: 'solid 1px rgb(50, 120, 100)',
+                backgroundColor: 'rgba(50, 120, 100, 0.4)',
+                marginRight: '10px',
+                marginBottom: '10px',
+                verticalAlign: 'bottom',
+              }}
+            ></div>
+            <div
+              style={{
+                display: 'inline-block',
+                width: '8px',
+                height: '8px',
+                borderRadius: '50%',
+                border: 'solid 1px rgb(50, 120, 100)',
+                backgroundColor: 'rgba(50, 120, 100, 0.4)',
+                marginRight: '10px',
+                marginBottom: '10px',
+                verticalAlign: 'bottom',
+              }}
+            ></div>
+          </div>
+
+          <p>
             <Words animate show={anim.entered}>
-              エアコン停止方法
+              そんな悩みを解決するのが、いつでもどこからでもエアコンを操作できるやばいエアコン操作アプリ&quot;AWECON&quot;!!!
             </Words>
-          </h3>
-          <List node="ol">
-            <li>
-              <p>
-                <Words animate show={anim.entered}>
-                  Arduinoに接続されている赤外線LEDを、エアコンに向け、「エアコンを停止」ボタンを押す。
-                </Words>
-              </p>
-            </li>
-          </List>
+          </p>
+          <p>
+            <Words animate show={anim.entered}>
+              この&quot;AWECON&quot;を使えば、パソコンからでもスマホからでも、ボタン一つで、簡単にエアコンを操作可能
+            </Words>
+          </p>
+          <p>
+            <Words animate show={anim.entered}>
+              暑い夏の日でも、寒い冬の日でも、動くのが面倒な時にだって大活躍!!
+            </Words>
+          </p>
         </div>
       )}
     </Project>
